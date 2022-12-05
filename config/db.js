9da29368114b2f38a9eb2d7373419d903d5124cb
Isam Makkie <makkieie@vcu.edu>
2022-12-05 17:28:21 -0500

    update1

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ import mysql from "serverless-mysql";
 
 const pool = mysql({
   config: {
-    host: "codeva.virginia-c4.cloud",
-    user: "jdleonard",
-    password: "Shout4_jdleonard_GO_ME",
-    port: 7833,
-    database: "project_codeva",
+    host: "cmsc508.com",
+    user: "makkieie",
+    password: "Shout4_makkieie_GOME",
+    port: 3016,
+    database: "202310_teams_team22",
   },
 });
 
