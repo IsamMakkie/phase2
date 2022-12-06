0d432fc666624fc72d2a297e7932103a93975365
Isam Makkie <makkieie@vcu.edu>
2022-12-06 16:14:26 -0500

    updated

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const pool = mysql({
     host: "cmsc508.com",
     user: "makkieie",
     password: "Shout4_makkieie_GOME",
-    port: 3016,
+    port: 3306,
     database: "202310_teams_team22",
   },
 });
